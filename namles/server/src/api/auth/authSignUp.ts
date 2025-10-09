c70d5c04870a0784de39bc07e6ebc8df5658ab40
Mootassam <Mootassame@gmail.com>
2025-10-09 06:46:20 -0700

    change the IP

--- namles/server/src/api/auth/authSignUp.ts
+++ namles/server/src/api/auth/authSignUp.ts
@@ -3,11 +3,6 @@ import AuthService from '../../services/auth/authService';
 
 export default async (req, res, next) => {
   try {
-
-
-
-
-
     const payload = await AuthService.signup(
       req.body.email,
       req.body.password,
